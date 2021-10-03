a8cc7dc4307a2127e6238d9cddcfaf5544d4455b
thereallucas98 <lucas1998david@gmail.com>
2021-10-03 10:44:45 -0300

    Finished Complemented Challenge

--- src/index.js
+++ src/index.js
@@ -11,18 +11,74 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User account not exist!" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request;
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ error: "User not allowed" });
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User account not exist!" });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "The id has a invalid format" });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  if (!todo) {
+    return response.status(404).json({ error: "Todo was not found" });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const { id } = request.params;
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "The id has a invalid format" });
+  }
+
+  const user = users.find(user => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User account not exist!" });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
